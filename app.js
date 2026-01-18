a21ed718cf8a5a7ff91bc331f4ee35a73867cd8f
roccostager <99860439+roccostager@users.noreply.github.com>
2026-01-18 12:13:32 +0100

    Create and route pages

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
-
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-	response.send('hello, word!');
+    response.sendFile(path.join(__dirname, 'pages/index.html'));
+});
+app.get('/about', (request, response) => {
+    response.sendFile(path.join(__dirname, 'pages/about-me.html'));
+});
+app.get('/contact', (request, response) => {
+    response.sendFile(path.join(__dirname, 'pages/contact.html'));
+});
+app.use((request, response) => {
+    response.sendFile(path.join(__dirname, 'pages/404.html'));
 });
 
 app.listen(port, error => {
